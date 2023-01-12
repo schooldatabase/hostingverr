b6a018309df934c08af2a8ef89215627af12c32c
vinod saini <andwerkumar@gmail.com>
2023-01-12 17:02:37 +0530

    tt
    tt

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-Python 3.10.4 -m pip install -r requirements.txt
-Python 3.10.4 manage.py collectstatic --noinput --clear
+Python 3.9 -m pip install -r requirements.txt
+Python 3.9 manage.py collectstatic --noinput --clear
 echo "BOILD END"
